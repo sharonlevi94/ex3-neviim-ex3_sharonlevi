739324c3544e79d017120d120ddb2ccc49339b56
sharonlevi94 <sharonlevy96@gmail.com>
2021-12-14 11:19:46 +0200

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+'use strict';
+export default {
+    getData: ()=>{
+
+    },
+    searchImages: ()=>{
+
+    },
+    clearSearch: ()=>{
+        console.log('clearSearch working')
+    },
+    startSlide: ()=>{
+        console.log('startSlide working')
+    },
+    stopSlide: ()=>{
+        console.log('stopSlide working')
+    },
+    saveImage: ()=>{
+        console.log('saveImage working')
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,37 @@
+'use strict';
+import functions from './functions.js'
+
+//-------------------------------------------------------------------
+let externalData = {
+    roverList: [],
+    cameraList: []
+}
+let searchParams = {
+    date: document.getElementById('date-input').value,
+    rover: document.getElementById('rover-input').value,
+    camera: document.getElementById('camera-input').value
+}
+const errorMessage = {
+    appearance: false,
+}
+
+//-------------define all event listeners---------------------------------------------------
+document.getElementById("search-images").addEventListener('submit', () => {
+    functions.searchImages()
+});
+document.getElementById("clear-search").addEventListener('click', () => {
+    functions.clearSearch()
+});
+document.getElementById("start-slide").addEventListener('click', () => {
+    functions.startSlide()
+});
+document.getElementById("stop-slide").addEventListener('click', () => {
+    functions.stopSlide()
+});
+/*document.getElementById('save-image').addEventListener('click', () => {
+    functions.saveImage()
+});*/
+document.addEventListener('DOMContentLoaded', () => {
+    functions.getData()
+});
+//-------------------------------------------------------------------
